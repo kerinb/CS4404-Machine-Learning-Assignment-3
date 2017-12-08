99a45d5695296d2efbb1a1a23992f3d2f93415d7
Breandan <kerinb@tcd.ie>
2017-12-08 10:24:11 +0000

    just added todo's

--- K_meansClustering/K_meansClustering.py
+++ K_meansClustering/K_meansClustering.py
@@ -4,3 +4,4 @@ import sklearn
 class K_meansClustering:
     def __init__(self):
         pass
+    # TODO - Imeplement Linear Regression and determine a couple of metrics
--- LinearRegression/LinearRegression.py
+++ LinearRegression/LinearRegression.py
@@ -4,3 +4,5 @@ import sklearn
 class LinearRegression:
     def __init__(self):
         pass
+
+    # TODO - Imeplement Linear Regression and determine a couple of metrics
